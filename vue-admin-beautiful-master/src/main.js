ab5dc83e4be376f0541074997c0b3a0e47d2b3dd
div404 <884631593@qq.com>
2020-07-07 18:54:56 +0800

    00

---
+++ vue-admin-beautiful-master/src/main.js
@@ -0,0 +1,17 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import Vue from "vue";
+import App from "./App";
+import store from "./store";
+import router from "./router";
+import "./plugins";
+import request from "@/utils/request";
+Vue.prototype.rq = request;
+Vue.config.productionTip = false;
+
+new Vue({
+  el: "#vue-admin-beautiful",
+  router,
+  store,
+  render: (h) => h(App),
+});
